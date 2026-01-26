2b1805d06f25fe8b82316d9186842ac52b3fc14f
frankPezo <frankanderpg.2000@gmail.com>
2026-01-26 02:06:49 -0500

    feat: Login Logic controller json

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -1,5 +1,83 @@
 <?php
   namespace App\controllers;
 
+  use App\models\LoginModel;
+  use App\models\AuditoriaLogModel;
+
+  class LoginController{
+     private $model;
+     private $modelLog;
+
+     public function __construct()
+     {
+        $this->model = new LoginModel();
+        $this->modelLog = new AuditoriaLogModel();
+     }
+
+      public function store():void{
+      //Leer json
+      $data = json_decode(file_get_contents('php://input'), true);
+      if($data === null){
+          $this->json(["status"=>"error", "msg"=>"Json inválido"], 400);
+          return;
+      }
+      //validación
+      if(empty($data['nombre_user']) || empty($data['correo_user']) || empty($data['password_user'])){
+          $this->json(['status'=>'error', 'msg'=> 'Datos incompletos'],400);
+          return;
+      }
+
+      $resultado = $this->model->create(
+        $data['nombre_user'], 
+        $data['correo_user'], 
+        $data['password_user']
+      );
+
+      if($resultado){
+        $this->json(['status'=> 'success', 'msg'=>'Usuario creado con éxito'], 201);
+      }else{
+        $this->json(['status'=> 'error', 'msg'=>'No se pudo crear usuario'], 500);
+      }
+    }
+
+
+
+    //login
+    public function login():void{
+      $data = json_decode(file_get_contents('php://input'),true);
+
+      if($data === null){
+          $this->json(["status"=>"error", "msg"=>"Json inválido"], 400);
+          return;
+      }
+
+      //Validación
+      if(empty($data['correo_user']) || empty($data['password_user'])){
+          $this->json(['status'=>'error','msg'=>'Credenciales incompletas'], 400);
+        return;
+      }
+
+      $user = $this->model->login($data['correo_user'], $data['password_user']);
+      if(!$user){
+          $this->json(['status'=>'error', 'msg'=>'Datos no coinciden'], 401);
+          return;
+      }
+
+      //Si todo va bien
+      session_start();
+      $_SESSION['id_user'] = $user['id_user'];
+      $_SESSION['nombre_user'] = $user['nombre_user'];
+
+      $this->modelLog->registrarLoginLog($user['id_user'], $user['nombre_user'], $_SERVER['REMOTE_ADDR']);
+        $this->json(['status'=>'success','msg'=>'Login correcto'], 200);
+    }
+
+    //Para que funcione el json
+    private function json($data, $statuscode=200):void{
+      http_response_code($statuscode);
+      header('Content-Type: application/json');
+      echo json_encode($data);
+    }
+  }
 
 ?>
--- app/models/loginModel.php
+++ app/models/loginModel.php
@@ -1,5 +1,49 @@
 <?php
  namespace App\models;
 
+ use PDO;
+ use App\database\Conexion;
+
+ class LoginModel{
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Conexion::contectar();
+    }
+
+    public function create(String $nombre_user, string $correo_user, string $password_user){
+        $passwordHash = password_hash($password_user, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO user(nombre_user, correo_user, password_user) VALUES(:nombre,:correo,:pasword)";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+           ':nombre' =>$nombre_user, 
+            ':correo'=>$correo_user, 
+           ':password' =>$passwordHash]);
+    }
+
+    public function login(string $correo_user, string $password_user){
+        $sql = "SELECT * FROM user WHERE correo_user =:correo_user LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':correo_user'=>$correo_user]);
+
+        $user =  $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if(!$user){
+            return false;
+        }
+
+        if(!password_verify($password_user,$user['password_user'])){
+            return false;
+        }
+
+          unset($user['password_user']); // nunca exponer el hash
+
+    return $user;
+    }
+
+
+ }
+
  
 ?>
